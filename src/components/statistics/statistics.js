abe86a80218a9c48bd8a1d2b4b9ed0b50c65593d
Olesia Yermosina <iermosina0711@gmail.com>
2023-12-14 11:46:06 +0200

    Improve line chart - added points, data and axes labels

--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -1,6 +1,7 @@
 import './statistics.scss';
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
+import {datePipe} from "../../helpers/datePipe";
 
 const MARGIN = { top: 30, right: 30, bottom: 50, left: 50 };
 
@@ -11,20 +12,20 @@ const Statistics = ({ type, period, data }) => {
     const boundsWidth = width - MARGIN.right - MARGIN.left;
     const boundsHeight = height - MARGIN.top - MARGIN.bottom;
 
-    // Y axis
+    // Будуємо вісь Y (вісь температури / вологості)
     const [min, max] = d3.extent(data, (d) => d[type]);
     const yScale = d3
         .scaleLinear()
         .domain([0, max || 0])
         .range([boundsHeight, 0]);
 
-    // X axis
+    // Будуємо вісь X (вісь часу)
     const xScale = d3
         .scaleTime()
         .domain(d3.extent(data, (d) => d.date))
         .range([ 0, boundsWidth ]);
 
-    // Render the X and Y axis using d3.js
+    // Рендеримо осі X та Y
     useEffect(() => {
         const svgElement = d3.select(axesRef.current);
         svgElement.selectAll("*").remove();
@@ -36,9 +37,43 @@ const Statistics = ({ type, period, data }) => {
 
         const yAxisGenerator = d3.axisLeft(yScale);
         svgElement.append("g").call(yAxisGenerator);
+
+        // Додаємо підпис осі X
+        svgElement.append("text")
+            .attr("text-anchor", "end")
+            .attr("x", width - 70)
+            .attr("y", height - 30)
+            .style("fill", d => "#9a6fb0")
+            .text('date'.toUpperCase() + ' axis ➜');
+
+        // Додаємо підпис осі Y
+        svgElement.append("text")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -MARGIN.left+10)
+            .attr("x", -MARGIN.top + 30)
+            .style("fill", d => "#9a6fb0")
+            .text(type.toUpperCase() + ' axis ➜');
+
+        // Додаємо підписи до точок на графіку
+        // При кліку на точку з'являється підпис значень в цій точці
+        const tooltip = svgElement.selectAll("circle")
+            .data(data)
+            .enter();
+        tooltip.append("text")
+            .attr("x", d => xScale(d.date))
+            .attr("y", d => yScale(d[type]))
+            .style("font-size", 14)
+            .style("opacity", 0)
+            .text(d => `${d[type]}${type === 'temperature' ? '°C' : '%'}; ${datePipe(d.date, period)}`)
+            .on("click", e => {
+                e.target.style.opacity = Number(e.target.style.opacity) === 1 ? 0 : 1;
+
+            });
+
     }, [xScale, yScale, boundsHeight, type, period, data]);
 
-    // Build the line
+    // Будуємо лінійний графік
     const lineBuilder = d3
         .line()
         .x((d) => xScale(d.date))
@@ -47,6 +82,20 @@ const Statistics = ({ type, period, data }) => {
     if (!linePath) {
         return null;
     }
+
+    // Додаємо точки на лінії графіку
+    const allCircles = data.map((item, i) => {
+        console.log()
+        return (
+            <circle
+                key={i}
+                cx={xScale(item.date)}
+                cy={yScale(item[type])}
+                r={4}
+                fill={"#9a6fb0"}
+            />
+        );
+    });
   
     return (
         <div id={'chart'}>
@@ -64,6 +113,7 @@ const Statistics = ({ type, period, data }) => {
                         fill="none"
                         strokeWidth={3}
                     />
+                    {allCircles}
                 </g>
                 <g
                     width={boundsWidth}
